452699780dfd8e1fb18ce33bc7e9494092f1a630
rdasan <rejidasan82@gmail.com>
2019-12-22 17:19:12 -0800

    Changing the GCD method from a recursive also to iterative also to facilitatte checking of negative numbers

--- fractionops.test/AdditionTests.cs
+++ fractionops.test/AdditionTests.cs
@@ -5,16 +5,27 @@ using Xunit;
 
 namespace fractionops.test
 {
-    public class FractionTests
+    public class AdditionTests
     {
        [Fact]
-        public void AdditionOperatorSimpleTest()
+        public void Simple()
         {
             var left = new Fraction(2,4);
             var right = new Fraction(1,4);
 
             var result = left + right;
             result.Numerator.Should().Be(3);
+            result.Denominator.Should().Be(4);
+        }
+
+        [Fact]
+        public void NegativeSimple()
+        {
+            var left = new Fraction(2, 4);
+            var right = new Fraction(-1, 4);
+
+            var result = left + right;
+            result.Numerator.Should().Be(1);
             result.Denominator.Should().Be(4);
         }
     }
--- fractionops/MathUtil.cs
+++ fractionops/MathUtil.cs
@@ -6,17 +6,18 @@ namespace fractionops
     {
         public static long GCD(long num1, long num2)
         {
-            if (num1 == 0)
-                return num2;
-            if (num2 == 0)
-                return num1;
+            num1 = Math.Abs(num1);
+            num2 = Math.Abs(num2);
 
-            return num1 > num2 ? GCD(num1 % num2, num2) : GCD(num1, num2 % num1);
-        }
+            while (num1 != 0 && num2 != 0)
+            {
+                if (num1 > num2)
+                    num1 %= num2;
+                else
+                    num2 %= num1;
+            }
 
-        public static long LCM(long num1, long num2)
-        {
-            return num1 * num2 / GCD(num1, num2);
+            return num1 == 0 ? num2 : num1;
         }
 
     }
