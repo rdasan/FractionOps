9466be04895e7d6a27c7481567a640fc72e76ac3
rdasan <rejidasan82@gmail.com>
2019-12-22 16:17:29 -0800

    Adding Util classes forcalculating GCD and LCM

---
+++ fractionops/MathUtil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace fractionops
+{
+    public static class MathUtil
+    {
+        public static long GCD(long num1, long num2)
+        {
+            num1 = Math.Abs(num1);
+            num2 = Math.Abs(num2);
+
+            //Usin Euclid's algorithm 
+            for (;;)
+            {
+                var remainder = num1 % num2;
+                if (remainder == 0)
+                    return num2;
+                num1 = num2;
+                num2 = remainder;
+            }
+        }
+
+        public static long LCM(long num1, long num2)
+        {
+            return num1 * num2 / GCD(num1, num2);
+        }
+
+    }
+}
--- fractionops/Models/Fraction.cs
+++ fractionops/Models/Fraction.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace fractionops.Models
 {
-    class Fraction
+    public class Fraction
     {
+        public long Numerator { get; set; }
+        public long Denominator { get; set; }
+
+        public Fraction(string fractionText)
+        {
+            var parts = fractionText.Split('/');
+            if (long.TryParse(parts[0], out long numerator))
+                Numerator = numerator;
+            if (long.TryParse(parts[1], out long denominator))
+                Denominator = denominator;
+            ReduceFraction();
+        }
+
+        private void ReduceFraction()
+        {
+            if (Denominator < 0)
+            {
+                Numerator = -Numerator;
+                Denominator = -Denominator;
+            }
+
+
+        }
     }
 }
